8377670ff6d956455dd027aaa9c0f1ecfcef8352
Ryan Amaral <ryan_amaral@live.com>
2021-06-13 14:04:11 -0300

    can draw sprite

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from graphics import *
+from sprite import Sprite
 
 win = GraphWin(width = 1024, height = 512)
 win.setCoords(0, 0, 1024, 512)
 win.setBackground("cyan3")
-mySquare = Rectangle(Point(200, 200), Point(400, 400)) 
-mySquare.draw(win) 
+#mySquare = Rectangle(Point(200, 200), Point(400, 400)) 
+#mySquare.draw(win)
+sprit = Sprite([(200,200), (400,200), (400,400), (200, 400)])
+sprit.translate(200,50)
+sprit.draw(win)
 win.getMouse()
--- sprite.py
+++ sprite.py
@@ -1,22 +1,32 @@
+import numpy as np
+from graphics import *
+
 class Sprite:
     
-    def __init__(self, shape, points=None):
-        self.shape = shape
-        if points is not None:
-            self.original_points = points
-            self.is_poly = True
-        else:
-            self.original_points = None
-            self.is_poly = False
+    def __init__(self, points):
+        # save shape reference for drawing and built in graphics stuff
+        #self.shape = Polygon(points)
+
+        # the position relative to the center
+        self.position = np.mean(points, axis=0)
+        
+        # save the points positions with the centroid moved to the middle
+        self.zeroed_points = points - self.position
 
     def rotate(self, deg):
         pass
 
     def translate(self, x, y):
-        pass
+        self.position += (x,y)
 
     def reset_rotation(self):
         pass
 
-    def set_positin(self, x, y):
+    def set_position(self, x, y):
         pass
+
+    def draw(self, window):
+        points0 = self.zeroed_points + self.position
+        points = [Point(x, y) for (x, y) in points0]
+        shape = Polygon(*points)
+        shape.draw(window)
